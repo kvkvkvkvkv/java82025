84c6d72f746b54df4c6d5d32087a1f60de840892
kvkvkvkvkv <kvsreenidhi@gmail.com>
2025-03-19 18:12:45 +0530

    map lombok

--- src/main/java/com/java/java8/Java8Application.java
+++ src/main/java/com/java/java8/Java8Application.java
@@ -1,13 +1,29 @@
 package com.java.java8;
 
+import com.java.java8.service.Streams;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.ConfigurableApplicationContext;
+
+import java.lang.reflect.Array;
+import java.util.Arrays;
 
 @SpringBootApplication
 public class Java8Application {
 
+	@Autowired
+	Streams streams;
+
 	public static void main(String[] args) {
-		SpringApplication.run(Java8Application.class, args);
+		ConfigurableApplicationContext context = SpringApplication.run(Java8Application.class, args);
+		Java8Application app = context.getBean(Java8Application.class); // Get an instance of the application
+		app.run();
+	}
+
+	public void run() {
+		streams.print(Arrays.asList(1,2,3,4,5,6,7,8,9));
+		streams.printCourses(Arrays.asList("Math", "Science", "English", "Physics", "Psycology", "Physiology"));
 	}
 
 }
--- src/main/java/com/java/java8/service/Streams.java
+++ src/main/java/com/java/java8/service/Streams.java
@@ -1,18 +1,54 @@
 package com.java.java8.service;
 
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.stereotype.Service;
+
 import java.util.List;
 
+@Slf4j
+@Service
 public class Streams {
 
     public void print(List<Integer> nums) {
 
         //static method reference
+        log.info("All Numbers");
         nums.forEach(Streams::printInteger); //Method Reference
+
+        log.info("All Numbers");
         nums.forEach(System.out::println); //inbuilt static method
+
+        log.info("Even Numbers");
         nums.stream()
-                .filter(ele -> ele%2==0) //boolean logic only
+                .filter(ele -> ele%2==0) //boolean logic only simplified rep of method
                 .forEach(System.out::println); //print even numbers
 
+        log.info("Odd Numbers");
+        nums.stream()
+                .filter(ele -> ele%2!=0)
+                .forEach(System.out::println);
+
+        log.info("Square Numbers");
+        nums.stream()
+                .filter(ele -> ele%2 == 0)
+                .map(ele -> ele*ele)
+                .forEach(System.out::println);
+    }
+
+    public void printCourses(List<String> courses) {
+
+        log.info("All Courses");
+        courses.forEach(System.out::println);
+
+        log.info("Only Phy");
+        courses.stream()
+                .filter(course -> course.contains("Phy"))
+                .forEach(System.out::println);
+
+        log.info(">7 length");
+        courses.stream()
+                .filter(course -> course.length()>7)
+                .forEach(System.out::println);
     }
 
     public static void printInteger(int val) {
